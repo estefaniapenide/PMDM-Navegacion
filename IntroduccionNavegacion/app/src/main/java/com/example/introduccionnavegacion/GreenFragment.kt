05edab424c37597984c1d5becc1cf8c37c5014e2
etef <estefaniapenide@gmail.com>
2022-01-27 22:05:47 +0100

    Algo más

--- IntroduccionNavegacion/app/src/main/java/com/example/introduccionnavegacion/GreenFragment.kt
+++ IntroduccionNavegacion/app/src/main/java/com/example/introduccionnavegacion/GreenFragment.kt
@@ -5,22 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.introduccionnavegacion.databinding.FragmentGreenBinding
 
 
 class GreenFragment : Fragment() {
+
+    private var _binding:FragmentGreenBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_green, container, false)
+        _binding= FragmentGreenBinding.inflate(inflater,container,false)
+        return binding.root
+        }
+
+
     }
 
-    companion object {
-
-        @JvmStatic
-        fun newInstance() = GreenFragment()
-
+    override fun onViewCreated(view: View,savedInstanceState: Bundle?){
+        super.onView
     }
 }
